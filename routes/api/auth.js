6d328013de4a4a4cd2e38a8457414235f2845b38
Travis Watson <watsontravis@protonmail.com>
2021-05-07 10:11:01 -0500

    Add auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route GET api/auth
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', async (req, res) => {
+    try {
+        const user = await (await User.findById(req.user.id)).isSelected('-password');
+        res.json(user);
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
