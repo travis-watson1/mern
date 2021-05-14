9278f2f9fe4050ed69752d97874168b04071cd94
Travis Watson <watsontravis@protonmail.com>
2021-05-14 14:33:15 -0500

    Add delete experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -189,4 +189,26 @@ router.put('/experience', [auth, [ check('title', 'Title is required').not().isE
     }
 });
 
+// @route DELETE api/profile/experience
+// @desc  Delete profile experience
+// @access Private
+
+router.delete('/experience/:exp_id', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
