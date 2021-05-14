8c83b794737562ee27e8bdd15860701b301a7845
Travis Watson <watsontravis@protonmail.com>
2021-05-14 14:14:46 -0500

    Add delete user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,4 +125,23 @@ router.get('/user/:user_id', async (req, res) =>{
         res.status(500).send('Server error');
     }
 });
+
+// @route DELETE api/profile
+// @desc Delete profile, user, and posts
+// @access Private
+router.delete('/', auth, async (req, res) =>{
+    try {
+        // @todo - remove users posts
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User removed.' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
