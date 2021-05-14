72a6d21652c15a05b9ef93b8363efbb16f50622c
Travis Watson <watsontravis@protonmail.com>
2021-05-14 13:51:32 -0500

    Add get all profiles and profiles by user id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,4 +92,37 @@ router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), 
       }
 });
 
+// @route GET api/profile
+// @desc Get all profiles
+// @access Public
+router.get('/', async (req, res) =>{
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by userid
+// @access Public
+router.get('/user/:user_id', async (req, res) =>{
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile){
+            return res.status(400).json({ msg: 'Profile not found.'});
+        }
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        if(error.kind === 'ObjectId'){
+            return res.status(400).json({ msg: 'Profile not found.'});
+        }
+        res.status(500).send('Server error');
+    }
+});
 module.exports = router;
